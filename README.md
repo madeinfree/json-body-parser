070fccfcd3178166088f7feb0c3814ac4612c5ea
Whien <sal95610@gmail.com>
2017-08-03 14:57:57 +0800

    first init

---
+++ README.md
@@ -0,0 +1,12 @@
+# JSON Body Parser
+
+nodeJS express server tiny body parser for json type only 320Byte
+
+* This is only for json type
+
+
+```javascript
+  const JSONBodyParser = require('json-body-parser')
+  app.use(JSONBodyParser)
+  // body { ... }
+```
---
+++ index.js
@@ -0,0 +1,25 @@
+const HTTP_METHOD = {
+  POST: 'POST'
+}
+
+function isPOST(method) {
+  return method === HTTP_METHOD.POST
+}
+
+function isJSON(type) {
+  return type.indexOf('application/json') > -1
+}
+
+module.exports = (req, res, next) => {
+  const method = req.method,
+        type = req.headers['content-type']
+  if (isPOST(method) && isJSON(type)) {
+    let body = ''
+    req.on('data', (chunk) => { body += chunk })
+    req.on('end', () => {
+      const parseBody = JSON.parse(body)
+      req.body = parseBody
+      next()
+    })
+  }
+}
